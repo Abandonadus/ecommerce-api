8258b3762b904bbf6f5558f928d173778fa2bbbd
Victor <paulo9.victor.silva@gmail.com>
2024-12-18 23:17:40 -0300

    chore: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('API dedicated to e-commerce management')
+    .setContact(
+      'Abandonadus - SoftwareHouse',
+      'www.example.com',
+      'abandonadus-softwarehouse@gmail.com',
+    )
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
