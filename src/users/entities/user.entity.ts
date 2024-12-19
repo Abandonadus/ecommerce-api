9cb5c43f6c978afc2ba0e87bea4f6744a041bae2
Victor <paulo9.victor.silva@gmail.com>
2024-12-18 23:18:26 -0300

    feat: add User Entity properties

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,9 @@
-export class User {}
+export class User {
+  user_id: string;
+  name: string;
+  email: string;
+  role_id: number;
+  password: string;
+  createdAt: string;
+  updatedAt: string;
+}
